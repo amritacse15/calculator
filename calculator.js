6465d02c73200befe066375b52f2d176ddb35197
amritacse15 <113686859+amritacse15@users.noreply.github.com>
2023-10-28 15:51:22 +0530

    calculator added

---
+++ calculator.js
@@ -0,0 +1,16 @@
+var buttons= document.getElementsByClassName("button");
+var display=document.getElementById("display");
+var operand1=0;
+var operand2=null;
+var operator=null;
+
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',function(){
+        var value=this.getAttribute('data-value');
+        if(value=='*'){
+            
+        }
+       
+
+    });
+}
